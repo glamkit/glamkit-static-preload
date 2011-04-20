c64b72ddb0e7e383f3226e70761e789414cf30e0
Aram Dulyan <aram00@gmail.com>
2011-04-20 18:00:04 +1000

    Turns out django.conf.urls.static only works with DEBUG=False, so is disabled during tests.

--- static_preload/tests/static_preload_test_app/urls.py
+++ static_preload/tests/static_preload_test_app/urls.py
@@ -1,8 +1,18 @@
+import re
+
 from django.conf import settings
-from django.conf.urls.static import static
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('static_preload.tests.static_preload_test_app.views',
     url(r'^tag_test/$', 'tag_test', name='tag_test'),
     url(r'^thingies/$', 'thingies', name='thingies'),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+) 
+
+# Serve static media (we can't use django.conf.urls.static, since it relies on 
+# DEBUG being False)
+urlpatterns += patterns('',
+    url(r'^%s/(?P<path>.*)$' % re.escape(settings.MEDIA_URL.strip('/')),
+        'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT},
+    ),
+)
