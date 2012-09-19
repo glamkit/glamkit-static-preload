e7c8ec31bdac2a7af02971afc1b77e13ef510fe8
Greg Turner <greg@interaction.net.au>
2012-09-19 21:23:27 +1000

    deprecating in favour of ixc

--- static_preload/models.py
+++
@@ -1,3 +0,0 @@
-# an app, sans models
-# a django pony through mist
-# both invisible
--- static_preload/tests/static_preload_test_app/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Thingie(models.Model):
-    content = models.CharField(max_length=50)
-    expires = models.DateTimeField(null=True)
